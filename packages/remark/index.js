c997e7342f07af2cae4d43529653a23da9c8c319
simov <simeonvelichkov@gmail.com>
2017-03-31 18:46:27 +0300

    Expose remark internals

--- packages/remark/index.js
+++ packages/remark/index.js
@@ -4,4 +4,9 @@ var unified = require('unified');
 var parse = require('remark-parse');
 var stringify = require('remark-stringify');
 
-module.exports = unified().use(parse).use(stringify).freeze();
+var remark = unified().use(parse).use(stringify).freeze();
+remark.unified = unified;
+remark.parse = parse;
+remark.stringify = stringify;
+
+module.exports = remark;
